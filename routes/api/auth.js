c7d4aaab2683bdab69cc22ff7630510d53bd140a
Senichkaa <super-kapanaiko406@ukr.net>
2023-12-01 18:00:31 +0200

    added hw05 with avatars and made logic

--- routes/api/auth.js
+++ routes/api/auth.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const authRouter = express.Router();
 const ctrl = require("../../controllers/auth");
-const { validateBody, authenticate } = require("../../middlewares");
+const { validateBody, authenticate, upload } = require("../../middlewares");
 const { registerAndLoginSchema } = require("../../models/user");
 
 authRouter.post(
@@ -14,5 +14,12 @@ authRouter.post("/login", validateBody(registerAndLoginSchema), ctrl.login);
 
 authRouter.get("/current", authenticate, ctrl.getCurrent);
 authRouter.post("/logout", authenticate, ctrl.logout);
+
+authRouter.patch(
+  "/avatars",
+  authenticate,
+  upload.single("avatar"),
+  ctrl.updateAvatar
+);
 
 module.exports = authRouter;
