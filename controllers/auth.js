d64d04552a2f124717226758b577714a66f42073
Senichkaa <super-kapanaiko406@ukr.net>
2023-12-08 16:26:02 +0200

    added verifies to hw and made 6th branch of hw

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,17 +1,18 @@
 require("dotenv").config();
 
 const { User } = require("../models/user");
-const { httpError, ctrlWrapper } = require("../helpers");
+const { httpError, ctrlWrapper, sendEmail } = require("../helpers");
 const bcrypt = require("bcrypt");
 const jwt = require("jsonwebtoken");
 const gravatar = require("gravatar");
 const fs = require("fs/promises");
 const Jimp = require("jimp");
 const path = require("path");
+const { nanoid } = require("nanoid");
 
 const avatarsDirectory = path.join(__dirname, "../", "public", "avatars");
 
-const { JWT_SECRET } = process.env;
+const { JWT_SECRET, BASE_URL } = process.env;
 
 const register = async (req, res) => {
   const { email, password } = req.body;
@@ -23,14 +24,58 @@ const register = async (req, res) => {
 
   const hashedPassword = await bcrypt.hash(password, 10);
   const avatarURL = gravatar.url(email);
+  const verificationToken = nanoid();
+
   const newUser = await User.create({
     ...req.body,
     password: hashedPassword,
     avatarURL,
+    verificationToken,
   });
+  const verifyEmail = {
+    to: email,
+    subject: "Verify email",
+    html: `<a target="_blank" href="${BASE_URL}/users/verify/${verificationToken}">Click here to verify</a>`,
+  };
+  await sendEmail(verifyEmail);
 
   res.status(201).json({
     user: { email: newUser.email, subscription: "starter" },
+  });
+};
+
+const verify = async (req, res) => {
+  const { verificationToken } = req.params;
+  const user = await User.findOne({ verificationToken });
+  if (!user) {
+    throw httpError(404, "User not found");
+  }
+  await User.findByIdAndUpdate(user._id, {
+    verify: true,
+    verificationToken: "",
+  });
+
+  res.json("Verification successfully");
+};
+
+const resendVerify = async (req, res) => {
+  const { email } = req.body;
+  const user = await User.findOne({ email });
+  if (!user) {
+    throw httpError(404, "User not found");
+  }
+  if (user.verify) {
+    throw httpError(400, "Verification has been passed already");
+  }
+  const verifyEmail = {
+    to: email,
+    subject: "Verify email",
+    html: `<a target="_blank" href="${BASE_URL}/users/verify/${user.verificationToken}">Click here to verify</a>`,
+  };
+  await sendEmail(verifyEmail);
+
+  res.json({
+    message: "Verification email sent",
   });
 };
 
@@ -39,6 +84,9 @@ const login = async (req, res) => {
   const user = await User.findOne({ email });
   if (!user) {
     throw httpError(409, "Email or password is incorrect");
+  }
+  if (!user.verify) {
+    throw httpError(404, "User not found");
   }
 
   const comparePassword = await bcrypt.compare(password, user.password);
@@ -98,4 +146,6 @@ module.exports = {
   logout: ctrlWrapper(logout),
   getCurrent: ctrlWrapper(getCurrent),
   updateAvatar: ctrlWrapper(updateAvatar),
+  verify: ctrlWrapper(verify),
+  resendVerify: ctrlWrapper(resendVerify),
 };
--- routes/api/auth.js
+++ routes/api/auth.js
@@ -2,7 +2,10 @@ const express = require("express");
 const authRouter = express.Router();
 const ctrl = require("../../controllers/auth");
 const { validateBody, authenticate, upload } = require("../../middlewares");
-const { registerAndLoginSchema } = require("../../models/user");
+const {
+  registerAndLoginSchema,
+  userEmailSchema,
+} = require("../../models/user");
 
 authRouter.post(
   "/register",
@@ -14,6 +17,8 @@ authRouter.post("/login", validateBody(registerAndLoginSchema), ctrl.login);
 
 authRouter.get("/current", authenticate, ctrl.getCurrent);
 authRouter.post("/logout", authenticate, ctrl.logout);
+authRouter.post("/verify", validateBody(userEmailSchema), ctrl.resendVerify);
+authRouter.get("/verify/:verificationToken", ctrl.verify);
 
 authRouter.patch(
   "/avatars",
